56870906e28bb81faa4d27cc917ad2b25c04c98c
mrandersonsmith <byronbb@shaw.ca>
2022-11-22 18:57:16 -0800

    Update f1update.sh

--- f1update.sh
+++ f1update.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
 cd ${HOME}
-wget -O ${HOME}/f1html.txt https://beta.f1mv.com/
-url=$(grep -o -E 'https?://[^"]+' ${HOME}/f1html.txt | grep linux)
+curl 'https://api.f1mv.com/api/v1/releases/latest' --compressed --output ${HOME}/f1html.txt
+
+url=$(grep -o -E 'https?://[^"]+' ${HOME}/f1html.txt | grep linux | head -1)
+
 
 wget -O f1viewer.zip $url
 unzip -o f1viewer.zip -d ${HOME}
 rm ${HOME}/f1viewer.zip
 rm ${HOME}/f1html.txt
+
 
